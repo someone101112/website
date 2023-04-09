dc950cbb44971afa0e5ec644720b4e60213e8f98
someone101112 <128602547+someone101112@users.noreply.github.com>
2023-04-09 11:00:04 +0300

    Update Music.js

--- src/sections/Music.js
+++ src/sections/Music.js
@@ -5,7 +5,7 @@ function Music(props) {
     return (
         <div className="music">
             <AnimationOnScroll offset={30} animateOnce={true} animateIn="animate__fadeIn">
-                <img title='Scan me' alt='We Are The People (Southstar Remix)' src={require('../assets/music/rickroll.svg').default} />
+                <img title='Scan me' alt='Never Gonna Give You Up(Rick Astley)' src={require('../assets/music/rickroll.svg').default} />
             </AnimationOnScroll>
             <p className='description'>Oh, and here's a <span className="song">song</span> I enjoy at the moment</p>
             <div className="contact-icons">
